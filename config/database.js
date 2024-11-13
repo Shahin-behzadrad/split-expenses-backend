a45e25e8e55aac73c88d5da1e753ebc0db9cc133
shahin behzadrad <shahinbzr2267@gmail.com>
2024-11-13 23:20:04 +0330

    fix: changing env variable's name

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
+    dialect: "mysql",
+    host: process.env.MYSQLHOST,
   }
 );
 
